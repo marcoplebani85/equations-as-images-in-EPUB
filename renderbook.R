27732527682a6048e25ae3b2deff46f01d6a22b1
Marco Plebani <marcoplebani85@users.noreply.github.com>
2026-03-01 12:10:07 +0100

    Add files via upload

--- renderbook.R
+++ renderbook.R
@@ -1,4 +1,33 @@
-# EPUB:
+
+# EPUB WITH FORMULAS AS .SVG:
+
+epub_webtex <- \(...) {
+	fmt <- bookdown::epub_book(...)
+	fmt$pandoc$args[format$pandoc$args == "--mathml"] <- "--webtex"
+	fmt
+}
+
+bookdown::render_book(
+  input = "index.Rmd",
+  output_format = epub_webtex(
+    fig_width = 5,
+    fig_height = 4,
+    dev = "png",
+    fig_caption = TRUE,
+    number_sections = TRUE,
+    toc = FALSE,
+    toc_depth = 2,
+    stylesheet = "style.css",
+    metadata = NULL,
+    chapter_level = 1,
+    md_extensions = NULL,
+    template = "default"
+  )
+)
+
+# Oh boy. What's up with this overwriting frenzy? EPUB readers overwriting EPUB style rules, bookdown overwriting instructions... Anyway: THANK YOU! Your fix worked. You're a gorram UNIT.
+
+# STANDARD EPUB:
 
 bookdown::render_book(input="index.Rmd",
 	bookdown::epub_book(
